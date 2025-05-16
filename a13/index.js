26e3b037c4d066552176bcef56c43cde64f6d4b3
charan210 <reddycharan2580@gmail.com>
2025-05-16 19:40:30 +0530

    updated

---
+++ a13/index.js
@@ -0,0 +1,6 @@
+const products=[
+    {id: 1,name:"Product 1",price:50},
+    {id: 2,name:"Product 2",price:40}
+    {id: 3,name:"Product 3",price:30}
+];
+products.forEach((product) => console.log(product.name));
