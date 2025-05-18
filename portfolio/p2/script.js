e8219a9d31fb0d7723ef30f868ae59c61727de97
charan210 <reddycharan2580@gmail.com>
2025-05-18 20:56:15 +0530

    updated

--- portfolio/p2/script.js
+++ portfolio/p2/script.js
@@ -4,21 +4,47 @@ const products = [
   { id: 3, name: "p3", price: 75 },
 ];
 
-const showProducts = () => {
- let str="";
- products.forEach((value)=>{
-    str+= `<div class="card">
-         <h2>${value.name}</h2>
-        <p>Price: ${value.price}</p>
-        <button onClick="adc(${value.id})">AddToCart</button>
-                </div>`;
- });
-  
+const cart = {};
 
-  document.getElementById("root").innerHTML=str;
+const addToCart = (id) => {
+  //insert id and quantity as 1 ie {1:1} if add button
+  // on box 1 is clicked, then if add button on box 3 is clicked then
+  //cart object should be {1:1,3:1}
+  // write code here....
+  if(cart[id]) cart[id]+=1;
+  else cart[id]=1;
+  console.log(cart);
 };
 
+const inc = (id)=>{
+cart[id]+=1;
+dispCart();
+};
+const dec = (id)=>{
+ cart[id]-=1;
+ dispCart();
+};
+const dispCart = () => {
+  let str = "<h2>Cart</h2>";
+  products.map((value) => {
+    cart[value.id] &&
+      (str += `<div>
+      ${value.name}-${value.price}-<button onClick="dec(${value.id})">-</button>${cart[value.id]}<button onClick="inc(${value.id})">+</button>-${value.price*cart[value.id]}
+     </div>
+    `);
+  });
+  root.innerHTML = str;
+};
 
-function adc(id){
-    alert(`Product ID:${id}Go ANd check,WHATs in your CArt????`)
-}
+const showProducts = () => {
+  let str = "";
+  products.map((value) => {
+    str += `<div>
+      <h3>${value.name}</h3>
+      <h4>${value.price}</h4>
+      <button onclick='addToCart(${value.id})'>Add to Cart</button>
+     </div>
+    `;
+  });
+  root.innerHTML = "<div class='row'>" + str + "</div>";
+};
